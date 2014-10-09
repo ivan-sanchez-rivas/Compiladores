85790235190fdaf8d654ea0daa8d274ce6df810e
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-10-08 20:41:48 -0700

    D# V1.1
    Se agrego la clase input

---
+++ DSharp/DSharp/input.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSharp
+{
+    public class input
+    {
+        private string textInput;
+        private int index;
+
+        public input(string Input)
+        {
+            this.textInput = Input;
+            index = 0;
+        }
+        private char manual
+        {
+            get
+            {
+                char c;
+                try
+                {
+                    c = textInput[index];
+                    index++;
+                }
+                catch (Exception)
+                {
+                    c = '@';
+                    return c;
+                }
+                return c;
+            }
+        }
+    }
+}
