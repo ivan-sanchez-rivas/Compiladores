50c1c6b957f308e44b665ed0230a6d910e975277
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-11-26 17:06:48 -0800

    D# V3.3

--- DSharp/DSharp/Program.cs
+++ DSharp/DSharp/Program.cs
@@ -11,9 +11,11 @@ namespace DSharp
     {
         static void Main(string[] args)
         {
-            List<SyntaxRules> rules = new List<SyntaxRules>();
-            var machine = new Machine();
-            var syntax = new SyntaxMachine();
+            while (true)
+            {
+                List<SyntaxRules> rules = new List<SyntaxRules>();
+                var machine = new Machine();
+                var syntax = new SyntaxMachine();
                 Console.WriteLine("Enter a source code input:");
                 string input = Console.ReadLine();
 
@@ -30,25 +32,27 @@ namespace DSharp
                     else
                     {
                         Console.WriteLine("The tokens are:\n");
-                        foreach (Token t in machine.Tokens){
+                        foreach (Token t in machine.Tokens)
+                        {
                             Console.WriteLine(t);
-                            
+
                         }
 
                         Console.WriteLine("\n");
                         Console.WriteLine("Analizador Sintactico: \n");
                         rules = syntax.TokenStrip(machine.Tokens);
-            
+
                         foreach (SyntaxRules sr in rules)
                         {
                             Console.WriteLine(sr);
                         }
-                            
-                        
+
+
                     }
                     Console.ReadLine();
                 }
-            
+                Console.Clear();
+            }
         }
     }
 }
---
+++ DSharp/DSharpLibrary/Variables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSharpLibrary
+{
+    public class Variables
+    {
+        string nombreVariable { get; set; }
+        string tipoVariable { get; set; }
+        string valorVariable { get; set; }
+
+        public Variables(string _nombreVariable, string _tipoVariable, string _valorVariable)
+        {
+            nombreVariable = _nombreVariable;
+            tipoVariable = _tipoVariable;
+            valorVariable = _valorVariable;
+        }
+    }
+}
